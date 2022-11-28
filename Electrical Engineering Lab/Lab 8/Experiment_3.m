18b8237aa96093f310d8ecf0fb33eb991f9c6e76
littleD3092 <drevild01801@gmail.com>
2022-11-29 00:07:44 +0800

    vault backup: 2022-11-29 00:07:44

--- Electrical Engineering Lab/Lab 8/Experiment_3.m
+++ Electrical Engineering Lab/Lab 8/Experiment_3.m
@@ -1,6 +1,12 @@
 sys = tf(1, [0.002, 0.01, 1])
 stepinfo(sys)
 
+% find delayTime
+[y, t] = step(sys, 2);
+differenceValues = abs(y - y(end)/2);
+[~, index] = min(differenceValues);
+delayTime = t(index)
+
 [y, t] = step(sys, 2);
 sserror=abs(1-y(end))
 
