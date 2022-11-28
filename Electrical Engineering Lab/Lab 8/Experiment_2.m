034cdab5a98799b274f757eb15489f70c1d7201d
littleD3092 <drevild01801@gmail.com>
2022-11-28 23:53:06 +0800

    vault backup: 2022-11-28 23:53:06

--- Electrical Engineering Lab/Lab 8/Experiment_2.m
+++ Electrical Engineering Lab/Lab 8/Experiment_2.m
@@ -1,4 +1,8 @@
 sys = tf(10, [0.2, 11])
 stepinfo(sys)
+
+[y, t] = step(sys, 0.5);
+sserror=abs(1-y(end))
+
 step(sys, 0.5)
 grid on
--- Electrical Engineering Lab/Lab 8/Experiment_3.m
+++ Electrical Engineering Lab/Lab 8/Experiment_3.m
@@ -1,4 +1,8 @@
 sys = tf(1, [0.002, 0.01, 1])
 stepinfo(sys)
+
+[y, t] = step(sys, 2);
+sserror=abs(1-y(end))
+
 step(sys, 2)
 grid on
