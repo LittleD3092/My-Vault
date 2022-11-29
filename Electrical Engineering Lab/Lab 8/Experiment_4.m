bec2d272143ab432f59658cacea6e41626a168b7
littleD3092 <drevild01801@gmail.com>
2022-11-29 19:33:09 +0800

    lab 8

--- Electrical Engineering Lab/Lab 8/Experiment_4.m
+++ Electrical Engineering Lab/Lab 8/Experiment_4.m
@@ -2,13 +2,13 @@ sys = tf([0.1, 1], [0.002, 0.11, 1])
 stepinfo(sys)
 
 % find delayTime
-[y, t] = step(sys, 0.5);
+[y, t] = step(sys, 2);
 differenceValues = abs(y - y(end)/2);
 [~, index] = min(differenceValues);
 delayTime = t(index)
 
-[y, t] = step(sys, 0.5);
+[y, t] = step(sys, 2);
 sserror=abs(1-y(end))
 
-step(sys, 0.5)
+step(sys, 2)
 grid on
