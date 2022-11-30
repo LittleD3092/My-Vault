59548ebaf693bddbebfdbdf40a660cb4a55c1cfd
littleD3092 <drevild01801@gmail.com>
2022-12-01 04:05:51 +0800

    vault backup: 2022-12-01 04:05:51

--- Automatic Control/HW3/hw5_15_method2/main.m
+++ Automatic Control/HW3/hw5_15_method2/main.m
@@ -3,16 +3,15 @@ clf;
 format short e
 options = [];
 
-y = zeros(61, 6);
 for i = 1:6
     m = 2*(i-1)*0.2;
     n = 1;
     x0 = [0 0]; % dunno what this is yet
     [t, x] = ode45('par_pass', [0 12], x0, options, m, n);
-    plot(t, x)
+    plot(t, (n) * x(:, 2) + (m) * x(:, 1) + 1)
     pause
 end
-pause
+%{
 grid
 title('Unit-Step Response Curves')
 xlabel('t Sec')
@@ -39,3 +38,4 @@ title('Three-Dimensional Plot of Unit-Step Response Curves using Command "mesh(y
 xlabel('Computation Time Points')
 ylabel('n, where n = 1, 2, 3, 4, 5, 6')
 zlabel('Outputs')
+%}
