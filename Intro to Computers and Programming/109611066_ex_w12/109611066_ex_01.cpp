8a91246c4f747ad00dd93503df9d8b1735f32699
LittleD3092 <81171284+LittleD3092@users.noreply.github.com>
2023-11-28 19:33:29 +0800

    Update 109611066_ex_01.cpp

--- Intro to Computers and Programming/109611066_ex_w12/109611066_ex_01.cpp
+++ Intro to Computers and Programming/109611066_ex_w12/109611066_ex_01.cpp
@@ -3,18 +3,22 @@
 #include <stdlib.h>
 #include <string.h>
 
-const int LABEL_LEN = 30;
-const char NAME_HDR[][LABEL_LEN] = {"NAME"};
+// #define USE_FILE
+// #define FORMATTED_INPUT
+// #define FORMATTED_OUTPUT
+
+#define LABEL_LEN 30
+char NAME_HDR[][LABEL_LEN] = {"NAME"};
 const int NAME_HDR_SIZE = 1;
-const char ID_HDR[][LABEL_LEN] = {"StudentID"};
+char ID_HDR[][LABEL_LEN] = {"StudentID"};
 const int ID_HDR_SIZE = 1;
-const char C[][LABEL_LEN] = {"Chinese"};
+char C[][LABEL_LEN] = {"Chinese"};
 const int C_SIZE = 1;
-const char M[][LABEL_LEN] = {"Math"};
+char M[][LABEL_LEN] = {"Math"};
 const int M_SIZE = 1;
-const char E[][LABEL_LEN] = {"English"};
+char E[][LABEL_LEN] = {"English"};
 const int E_SIZE = 1;
-const char CS[][LABEL_LEN] = {"ComputerScience"};
+char CS[][LABEL_LEN] = {"ComputerScience"};
 const int CS_SIZE = 1;
 
 typedef struct _grade {
@@ -46,6 +50,7 @@ void clearStdinLine()
 int main()
 {
     // open file
+#ifdef USE_FILE
     FILE* fp;
     fp = fopen("input.txt", "r");
     if (fp == NULL) 
@@ -97,24 +102,47 @@ int main()
                 fscanf(fp, "%d", &students[i].student_grade.computer_science);
         }
     }
+#endif
+#ifndef USE_FILE
+    const int numEntries = 5;
+    const int numCols = 6;
+    const int numQueries = 4;
+    struct student students[] = {
+        {"Bob", 309553045, 0, 10, 20, 30},
+        {"Kevin", 309551116, 100, 100, 50, 100},
+        {"Dave", 309551110, 59, 80, 43, 70},
+        {"Steve", 309552003, 95, 33, 10, 99},
+        {"Stuart", 309552052, 80, 40, 70, 80}
+    };
+#endif
 
     // read queries
+#ifdef FORMATTED_OUTPUT
     printf("Input queries score:\n");
+#endif
     char** queryType = (char**)malloc(sizeof(char*) * numQueries);
     int* queryScore = (int*)malloc(sizeof(int) * numQueries);
     for(int i = 0; i < numQueries; i++)
     {
         queryType[i] = (char*)malloc(sizeof(char) * 5);
         int j = 0;
-        while(true)
+        while(1)
         {
             char c = getchar();
+#ifdef FORMATTED_INPUT
             if(c == ':')
                 break;
             else if(c == ' ')
                 continue;
             else
                 queryType[i][j++] = c;
+#endif
+#ifndef FORMATTED_INPUT
+            if(c == ' ')
+                break;
+            else
+                queryType[i][j++] = c;
+#endif
         }
         queryType[i][j] = '\0';
         scanf("%d", &queryScore[i]);
@@ -132,14 +160,19 @@ int main()
     padding[1] = maxLen - strlen(M[0]);
     padding[2] = maxLen - strlen(E[0]);
     padding[3] = maxLen - strlen(CS[0]);
-    printf("\nResult:\n");
+#ifdef FORMATTED_OUTPUT
+    printf("\n");
+#endif
+    printf("Result:\n");
     for(int i = 0; i < numQueries; i++)
     {
         if(strcmp(queryType[i], "C") == 0)
         {
             printf("%s", C[0]);
+#ifdef FORMATTED_OUTPUT
             for(int j = 0; j < padding[0]; j++)
                 printf(" ");
+#endif
             printf(":");
             for(int j = 0; j < numEntries; j++)
                 if(students[j].student_grade.Chinese < queryScore[i])
@@ -148,8 +181,10 @@ int main()
         else if(strcmp(queryType[i], "M") == 0)
         {
             printf("%s", M[0]);
+#ifdef FORMATTED_OUTPUT
             for(int j = 0; j < padding[1]; j++)
                 printf(" ");
+#endif
             printf(":");
             for(int j = 0; j < numEntries; j++)
                 if(students[j].student_grade.Math < queryScore[i])
@@ -158,8 +193,10 @@ int main()
         else if(strcmp(queryType[i], "E") == 0)
         {
             printf("%s", E[0]);
+#ifdef FORMATTED_OUTPUT
             for(int j = 0; j < padding[2]; j++)
                 printf(" ");
+#endif
             printf(":");
             for(int j = 0; j < numEntries; j++)
                 if(students[j].student_grade.English < queryScore[i])
@@ -168,8 +205,10 @@ int main()
         else if(strcmp(queryType[i], "CS") == 0)
         {
             printf("%s", CS[0]);
+#ifdef FORMATTED_OUTPUT
             for(int j = 0; j < padding[3]; j++)
                 printf(" ");
+#endif
             printf(":");
             for(int j = 0; j < numEntries; j++)
                 if(students[j].student_grade.computer_science < queryScore[i])
@@ -179,7 +218,9 @@ int main()
     }
 
     // close file
+#ifdef USE_FILE
     fclose(fp);
+#endif
 
     return 0;
 }
